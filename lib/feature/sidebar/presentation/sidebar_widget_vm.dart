1fe708ac68544ddaab37ca4b955a2d0afefc7cbf
Bogdan Onischenko <bogdan-dev@bk.ru>
2024-04-15 16:50:34 +0300

    исправил баг с bottomSheet

--- lib/feature/sidebar/presentation/sidebar_widget_vm.dart
+++ lib/feature/sidebar/presentation/sidebar_widget_vm.dart
@@ -1,11 +1,14 @@
 import 'package:dot_marketplace/feature/settings/presentation/settings_modal_bs.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SidebarWidgetViewModel {
   void onSettingsTap(BuildContext context) {
     showModalBottomSheet(
+      useRootNavigator: true,
       context: context,
       builder: (context) => SettingsModalBottomSheet(),
     );
+    context.pop();
   }
 }
